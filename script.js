6b4dc178314146f84c0c1afc8349d4c36c9592d0
iasmymsimoes <isl8@aluno.ifnmg.edu.br>
2026-08-07 13:45:46 -0300

    Corrige rotacao da primeira pagina

--- script.js
+++ script.js
@@ -143,7 +143,8 @@ function renderPage(num) {
  statusOcr.style.display = 'none';
 
  state.pdfDoc.getPage(num).then(page => {
- const viewport = page.getViewport({ scale: state.scale });
+const rotacao = num === 1 ? (page.rotate + 180) % 360 : page.rotate;
+const viewport = page.getViewport({ scale: state.scale, rotation: rotacao });
 
  canvas.height = viewport.height;
  canvas.width = viewport.width;
